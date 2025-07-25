9f98cfeb2ab867ef912a6d0639bc3b36b0f2cb39
Walace Silva <wsilva.emp@gmail.com>
2025-07-24 21:07:02 -0300

    feat: adjust layout dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,10 @@
-import Layout from "@/components/layout"
-import DashboardSection from "@/components/section/dashboard"
+import Layout from "@/components/layout";
+import DashboardSection from "@/components/section/dashboard";
 
 export default function Home() {
 	return (
 		<Layout breadcrumbTitle="Dashboard">
 			<DashboardSection />
 		</Layout>
-	)
+	);
 }
-
