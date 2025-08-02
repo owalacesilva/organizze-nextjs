bfd912a02ce1bd95cbce083eb7ba049b8a88dc40
Walace Silva <wsilva.emp@gmail.com>
2025-08-01 23:12:10 -0300

    feat: update nav menu

--- components/elements/user-nav.jsx
+++ components/elements/user-nav.jsx
@@ -1,24 +1,30 @@
-"use client"
+"use client";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { LogOut, Settings, User, Wallet } from "lucide-react"
-import Link from "next/link"
+} from "@/components/ui/dropdown-menu";
+import { LogOut, Settings, User, Wallet } from "lucide-react";
+import Link from "next/link";
 
 export function UserNav() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="ghost" className="h-9 w-9 rounded-full hover:bg-transparent">
+				<Button
+					variant="ghost"
+					className="h-9 w-9 rounded-full hover:bg-transparent"
+				>
 					<Avatar className="h-8 w-8">
-						<AvatarImage src="/images/avatar/1.jpg?height=32&width=32" alt="@hafsa" />
+						<AvatarImage
+							src="/images/avatar/1.jpg?height=32&width=32"
+							alt="@hafsa"
+						/>
 						<AvatarFallback>HH</AvatarFallback>
 					</Avatar>
 				</Button>
@@ -26,7 +32,10 @@ export function UserNav() {
 			<DropdownMenuContent className="w-56" align="end">
 				<div className="flex items-center gap-3 p-3">
 					<Avatar className="h-9 w-9">
-						<AvatarImage src="/images/avatar/1.jpg?height=36&width=36" alt="@hafsa" />
+						<AvatarImage
+							src="/images/avatar/1.jpg?height=36&width=36"
+							alt="@hafsa"
+						/>
 						<AvatarFallback>HH</AvatarFallback>
 					</Avatar>
 					<div className="flex flex-col">
@@ -39,12 +48,6 @@ export function UserNav() {
 					<DropdownMenuItem className="flex items-center gap-3 p-3 cursor-pointer">
 						<User className="h-4 w-4" />
 						<span>Profile</span>
-					</DropdownMenuItem>
-				</Link>
-				<Link href="/wallets" className="w-full">
-					<DropdownMenuItem className="flex items-center gap-3 p-3 cursor-pointer">
-						<Wallet className="h-4 w-4" />
-						<span>Wallets</span>
 					</DropdownMenuItem>
 				</Link>
 				<Link href="/settings" className="w-full">
@@ -62,6 +65,5 @@ export function UserNav() {
 				</Link>
 			</DropdownMenuContent>
 		</DropdownMenu>
-	)
+	);
 }
-
